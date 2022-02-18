84b086757efc36c37fb84585dc71577634f43cce
DiegoJRR <diegojrr00@gmail.com>
2022-02-18 07:37:47 -0600

    options for chromedriver and chromedrivermanager

--- questionnaire.py
+++ questionnaire.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 from PIL import Image
+from webdriver_manager.chrome import ChromeDriverManager
 
 from flask_restful import Resource
 from flask import send_file, request
@@ -16,9 +17,18 @@ class AutoQuestionnaire(Resource):
     def get(self):
         options = Options()
         options.headless = True
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu-sandbox')
+        options.add_argument("--single-process")
+        options.add_argument(
+        '"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"')
+        options.add_argument("--log-level=0")
+        options.add_argument("--single-process")
+        options.add_argument("--ignore-certificate-errors")
 
-        chromedriver = 'chromedriver.exe'
-        browser = webdriver.Chrome(chromedriver, options=options)
+        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         browser.get('https://flpnwc-aj982psom1.dispatcher.us3.hana.ondemand.com/sites/regresoseguro')
         browser.implicitly_wait(30)
 
