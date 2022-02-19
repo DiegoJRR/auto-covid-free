cc42c98f9ae62d3ee47b01a9ce6934c6220321df
DiegoJRR <diegojrr00@gmail.com>
2022-02-18 23:15:38 -0600

    alias for chrome binary

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg-deb -xv google-chrome-stable_current_amd64.deb .
+rm google-chrome-stable_current_amd64.deb
 
-gunicorn app:app --worker-class gevent --timeout 600
+alias google-chrome="opt/google/chrome/chrome"
+
+gunicorn app:app --timeout 600
